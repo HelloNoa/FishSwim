e2ca64590136af4daf6df6d9e32b2fa452345d85
HelloNoa <ohyoungwoo824@gmail.com>
2023-06-29 21:58:48 +0900

    feat: first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("game")
+const ctx = canvas.getContext("2d");
+// const image = document.getElementById("source");
+//
+// image.addEventListener("load", (e) => {
+//     ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
+// });
+window.onload=()=>{
+    console.log("onload")
+}
