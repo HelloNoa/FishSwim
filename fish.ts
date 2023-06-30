ec1f2ea8deeed394883c3d4cface09fd94dee16c
노아 <ohyoungwoo824@gmail.com>
2023-06-30 21:34:17 +0900

    feat: change typescript script

--- fish.ts
+++ fish.ts
@@ -1,37 +1,53 @@
+type T_Vecter = {
+    up:boolean;
+    left:boolean;
+    right:boolean;
+    down:boolean;
+}
+
 export class Fish {
-    _x;
-    _y;
-    _moveSpeed;
-    _width;
-    _height;
-    _flip;
-    _vecter;
-    constructor(_x = 0, _y = 0, _moveSpeed = 1, _width = 10, _height = 10) {
+    private _x: number;
+    private _y: number;
+    private _moveSpeed: number;
+    private _width: number;
+    private _height: number;
+    private _flip: boolean;
+    private _vecter: T_Vecter;
+
+    constructor(_x: number = 0, _y: number = 0, _moveSpeed: number = 1, _width: number = 10, _height: number = 10) {
         this.x = _x;
         this.y = _y;
         this.moveSpeed = _moveSpeed;
         this.width = _width;
         this.height = _height;
+
         this.flip = false;
+
         this.vecter = {
             up: false,
             left: false,
             right: false,
             down: false,
-        };
+        }
     }
+
     get x() {
         return this._x;
     }
+
     set x(x) {
         this._x = x;
     }
+
+
     get y() {
         return this._y;
     }
-    set y(y) {
+
+    set y(y:number) {
         this._y = y;
     }
+
     setXY(x, y) {
         if (x !== null) {
             this.x = x;
@@ -40,40 +56,53 @@ export class Fish {
             this.y = y;
         }
     }
+
     addForceXY(forceX = 0, forceY = 0) {
         this.x += forceX;
         this.y += forceY;
     }
+
+
     get moveSpeed() {
         return this._moveSpeed;
     }
+
     set moveSpeed(moveSpeed) {
         this._moveSpeed = moveSpeed;
     }
+
     get width() {
         return this._width;
     }
+
     set width(width) {
         this._width = width;
     }
+
     get height() {
         return this._height;
     }
+
     set height(height) {
         this._height = height;
     }
+
     get flip() {
         return this._flip;
     }
+
     set flip(flip) {
         this._flip = flip;
     }
+
     get vecter() {
         return this._vecter;
     }
+
     set vecter(vecter) {
         this._vecter = vecter;
     }
+
     setVecter(key, status) {
         switch (key) {
             case "w":
@@ -83,8 +112,7 @@ export class Fish {
                 this.vecter.left = status;
                 if (status) {
                     this.flip = false;
-                }
-                else if (this.vecter.right) {
+                } else if (this.vecter.right) {
                     this.flip = true;
                 }
                 break;
@@ -95,13 +123,13 @@ export class Fish {
                 this.vecter.right = status;
                 if (status) {
                     this.flip = true;
-                }
-                else if (this.vecter.left) {
+                } else if (this.vecter.left) {
                     this.flip = false;
                 }
                 break;
             default:
-                break;
+                break
         }
     }
+
 }
--- index.ts
+++ index.ts
@@ -1,42 +1,48 @@
-import { Fish } from './fish.js';
-import { Enemy } from './enemy.js';
-export const canvas = document.getElementById("game");
+import {Fish} from './fish.js';
+import {Enemy} from './enemy.js';
+
+
+export const canvas = document.getElementById("game") as HTMLCanvasElement;
 export const ctx = canvas.getContext("2d");
 var img = new Image();
 img.src = "fish.png";
 const Player = new Fish(200, 300, 1, 100, 50);
+
 img.onload = () => {
     ctx.drawImage(img, Player.x, Player.y, Player.width, Player.height);
-};
+}
+
 const enemyImg = new Image();
-enemyImg.src = "fish.png";
-const EnemyFish = new Enemy(0, 0, 1, 30, 15);
+enemyImg.src = "fish.png"
+const EnemyFish = new Enemy(0, 0, 1, 30, 15)
 window.onkeydown = (e) => {
     const key = e.key.toLocaleLowerCase();
-    Player.setVecter(key, true);
+    Player.setVecter(key, true)
 };
 window.onkeyup = (e) => {
     const key = e.key.toLocaleLowerCase();
-    Player.setVecter(key, false);
+    Player.setVecter(key, false)
 };
+
 window.onload = () => {
     setDPI();
-    console.log("onload");
-    setInterval(move, 10);
+    console.log("onload")
+    setInterval(move, 10)
     // setTimeout(() => {
     //     location.reload();
     // }, 1000)
-};
+}
 const move = () => {
     if (Player.vecter.up && Player.y >= 0)
-        Player.addForceXY(0, -Player.moveSpeed);
+        Player.addForceXY(0, -Player.moveSpeed)
     if (Player.vecter.left && Player.x >= 0)
-        Player.addForceXY(-Player.moveSpeed, 0);
+        Player.addForceXY(-Player.moveSpeed, 0)
     if (Player.vecter.right && Player.x <= canvas.width - Player.width)
-        Player.addForceXY(Player.moveSpeed, 0);
+        Player.addForceXY(Player.moveSpeed, 0)
     if (Player.vecter.down && Player.y <= canvas.height - Player.height)
-        Player.addForceXY(0, Player.moveSpeed);
+        Player.addForceXY(0, Player.moveSpeed)
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     EnemyFish.move();
     ctx.save();
     ctx.translate(canvas.width / 2 + EnemyFish.width / 2, canvas.height / 2 - EnemyFish.height / 2);
@@ -45,30 +51,37 @@ const move = () => {
     ctx.translate(canvas.width / 2 - EnemyFish.width / 2, -canvas.height / 2 + EnemyFish.height / 2);
     ctx.drawImage(img, EnemyFish.x, EnemyFish.y, EnemyFish.width, EnemyFish.height);
     ctx.restore();
+
     if (Player.flip) {
         ctx.drawImage(img, Player.x, Player.y, Player.width, Player.height);
-    }
-    else {
+    } else {
         ctx.save();
         // ctx.translate(canvas.width / 2 - Player.width / 2, canvas.height / 2 - Player.height / 2);
         ctx.translate(canvas.width / 2 + Player.width / 2, canvas.height / 2 - Player.height / 2);
         ctx.scale(-1, 1);
-        ctx.translate(canvas.width / 2 - Player.width / 2, -canvas.height / 2 + Player.height / 2);
+        ctx.translate(canvas.width / 2 - Player.width/2, -canvas.height / 2 + Player.height / 2);
         ctx.drawImage(img, -Player.x, Player.y, Player.width, Player.height);
         ctx.restore();
     }
-};
+
+}
+
 const setDPI = () => {
     // 디스플레이 크기 설정 (css 픽셀)
-    const width = 696;
-    const height = 564;
+    const width = 696
+    const height = 564
     canvas.style.width = `${width}px`;
     canvas.style.height = `${height}px`;
+
     // 메모리에 실제 크기 설정 (픽셀 밀도를 고려하여 크기 조정)
     const dpr = window.devicePixelRatio;
+
     canvas.width = width * dpr;
     canvas.height = height * dpr;
+
     const ctx = canvas.getContext('2d');
+
     // CSS에서 설정한 크기와 맞춰주기 위한 scale 조정
     ctx.scale(dpr, dpr);
-};
+
+}
