f6e6052aeb8d790621437ddcb8f5456112e18a74
HelloNoa <ohyoungwoo824@gmail.com>
2023-07-21 00:54:08 +0900

    fix: 천적 추가

--- enemy.js
+++ enemy.js
@@ -1,5 +1,5 @@
 import { Fish } from './fish.js';
-import { FishWidth, height, IMAGE, Player, width } from "./index.js";
+import { FishHeight, FishWidth, height, IMAGE, Player, width } from "./index.js";
 import { d2d } from "./util/index.js";
 export class Enemy extends Fish {
     constructor(x = 0, y = 0, moveSpeed = 1, width = 10, height = 10, img = IMAGE.green) {
@@ -31,11 +31,33 @@ export class Enemy extends Fish {
     }
     crash() {
         if (d2d(Player.mx, Player.my, this.mx, this.my) <= Math.max(this.height, Player.height) * 0.3) {
-            this.setRandomStartingPosition();
             const delta = this.width / FishWidth;
-            Player.width *= (1 + delta * 0.05);
-            Player.height *= (1 + delta * 0.05);
-            console.log('먹었당');
+            const PlayerDelta = Player.width / FishWidth;
+            console.log(PlayerDelta);
+            if (delta > PlayerDelta) {
+                console.log('먹혔당');
+                Player.width = Math.max(FishWidth * 0.05, Player.width * 0.99);
+                Player.height = Math.max(FishHeight * 0.05, Player.height * 0.99);
+            }
+            else {
+                this.setRandomStartingPosition();
+                Player.width *= (1 + delta * 0.1);
+                Player.height *= (1 + delta * 0.1);
+                console.log('먹었당');
+            }
+            document.querySelectorAll('ul.fishList li').forEach(e => e.classList.remove('active'));
+            if (PlayerDelta >= 0.1)
+                document.querySelectorAll('ul.fishList li')[0].classList.add('active');
+            if (PlayerDelta >= 0.2)
+                document.querySelectorAll('ul.fishList li')[1].classList.add('active');
+            if (PlayerDelta >= 0.4)
+                document.querySelectorAll('ul.fishList li')[2].classList.add('active');
+            if (PlayerDelta >= 0.5)
+                document.querySelectorAll('ul.fishList li')[3].classList.add('active');
+            if (PlayerDelta >= 0.6)
+                document.querySelectorAll('ul.fishList li')[4].classList.add('active');
+            if (PlayerDelta >= 1)
+                document.querySelectorAll('ul.fishList li')[5].classList.add('active');
         }
     }
     update() {
--- enemy.ts
+++ enemy.ts
@@ -1,5 +1,5 @@
 import {Fish} from './fish.js';
-import {FishWidth, height, IMAGE, Player, width} from "./index.js";
+import {FishHeight, FishWidth, height, IMAGE, Player, width} from "./index.js";
 import {d2d} from "./util/index.js";
 
 export class Enemy extends Fish {
@@ -33,11 +33,26 @@ export class Enemy extends Fish {
 
     crash() {
         if (d2d(Player.mx, Player.my, this.mx, this.my) <= Math.max(this.height, Player.height) * 0.3) {
-            this.setRandomStartingPosition();
             const delta = this.width / FishWidth
-            Player.width *= (1 + delta * 0.05);
-            Player.height *= (1 + delta * 0.05);
-            console.log('먹었당');
+            const PlayerDelta = Player.width / FishWidth;
+            console.log(PlayerDelta)
+            if (delta > PlayerDelta) {
+                console.log('먹혔당');
+                Player.width = Math.max(FishWidth * 0.05, Player.width * 0.99);
+                Player.height = Math.max(FishHeight * 0.05, Player.height * 0.99);
+            } else {
+                this.setRandomStartingPosition();
+                Player.width *= (1 + delta * 0.1);
+                Player.height *= (1 + delta * 0.1);
+                console.log('먹었당');
+            }
+            document.querySelectorAll('ul.fishList li').forEach(e => e.classList.remove('active'));
+            if (PlayerDelta >= 0.1) document.querySelectorAll('ul.fishList li')[0].classList.add('active')
+            if (PlayerDelta >= 0.2) document.querySelectorAll('ul.fishList li')[1].classList.add('active')
+            if (PlayerDelta >= 0.4) document.querySelectorAll('ul.fishList li')[2].classList.add('active')
+            if (PlayerDelta >= 0.5) document.querySelectorAll('ul.fishList li')[3].classList.add('active')
+            if (PlayerDelta >= 0.6) document.querySelectorAll('ul.fishList li')[4].classList.add('active')
+            if (PlayerDelta >= 1) document.querySelectorAll('ul.fishList li')[5].classList.add('active')
         }
 
     }
