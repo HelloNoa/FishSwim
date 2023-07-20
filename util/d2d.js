701ceaccaae75b610822b397525936073d42457d
노아 <ohyoungwoo824@gmail.com>
2023-07-20 20:01:50 +0900

    feat: add d2d func

---
+++ util/d2d.js
@@ -0,0 +1,6 @@
+export const d2d = (d1x, d1y, d2x, d2y) => {
+    const x = d2x - d1x;
+    const y = d2y - d1y;
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+};
+//# sourceMappingURL=d2d.js.map
